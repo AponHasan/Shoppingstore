7a8f0d3de086f69b376c9e18090fde90095a9d65
AponHasan <mozammal.bubt89@gmail.com>
2018-01-08 00:21:19 +0600

    Create shopping store project

---
+++ Shoppingstore/Controllers/StoreController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shoppingstore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "Hello Index";
+        }
+        public string browser(string category)
+        {
+            string message = HttpUtility.HtmlEncode("Showing category "+category);
+            return message;
+        }
+        public string Details(int id)
+        {
+            string message = "store details" + id;
+            return message;
+        }
+    }
+}
